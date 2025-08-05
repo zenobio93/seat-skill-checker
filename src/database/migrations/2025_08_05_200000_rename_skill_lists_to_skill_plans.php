4da17f1de854e0784fb956cd657986786aa00d45
Christian Hellmund <christian.hellmund@check24.de>
2025-08-05 20:49:27 +0200

    Fix migration

--- src/database/migrations/2025_08_05_200000_rename_skill_lists_to_skill_plans.php
+++ src/database/migrations/2025_08_05_200000_rename_skill_lists_to_skill_plans.php
@@ -14,14 +14,14 @@ return new class extends Migration
     public function up()
     {
         // Rename skill_plans table to skill_plans
-        Schema::rename('skill_plans', 'skill_plans');
+        Schema::rename('skill_lists', 'skill_plans');
 
         // Rename skill_plan_requirements table to skill_plan_requirements
-        Schema::rename('skill_plan_requirements', 'skill_plan_requirements');
+        Schema::rename('skill_list_requirements', 'skill_plan_requirements');
 
         // Update foreign key column name in skill_plan_requirements table
         Schema::table('skill_plan_requirements', function (Blueprint $table) {
-            $table->renameColumn('skill_plan_id', 'skill_plan_id');
+            $table->renameColumn('skill_list_id', 'skill_plan_id');
         });
     }
 
@@ -34,13 +34,13 @@ return new class extends Migration
     {
         // Revert foreign key column name in skill_plan_requirements table
         Schema::table('skill_plan_requirements', function (Blueprint $table) {
-            $table->renameColumn('skill_plan_id', 'skill_plan_id');
+            $table->renameColumn('skill_plan_id', 'skill_list_id');
         });
 
         // Revert skill_plan_requirements table to skill_plan_requirements
-        Schema::rename('skill_plan_requirements', 'skill_plan_requirements');
+        Schema::rename('skill_plan_requirements', 'skill_list_requirements');
 
         // Revert skill_plans table to skill_plans
-        Schema::rename('skill_plans', 'skill_plans');
+        Schema::rename('skill_plans', 'skill_lists');
     }
 };
